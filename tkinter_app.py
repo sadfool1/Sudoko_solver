eb59bd5af7b9666dfeec8c6e98e30dfb1df9b696
James <takw14@gmail.com>
2020-09-04 22:46:13 +0800

    Build Tkinter

---
+++ tkinter_app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  4 22:25:12 2020
+
+@author: jameselijah
+"""
+
+import sys
+import traceback
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from random import randint
+import random
+from tkinter import *
+import tkinter as tk
+from mpl_toolkits.mplot3d import Axes3D #imports the 3D 
+import time
+from tkinter import TclError
+
+class GAME_CONTROLLER():
